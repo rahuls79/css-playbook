9c13c9b9852be0efa7fda653fbe8e3bb41f4d747
rahuls79 <rahul.rahulr.sinha@gmail.com>
2023-12-02 20:25:18 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Heart from "./components/Heart";
+
+function App() {
+  return (
+    <div className="App">
+      <Heart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heart.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Heart.css";
+
+const Heart = () => {
+  return <div className="heart"></div>;
+};
+
+export default Heart;
